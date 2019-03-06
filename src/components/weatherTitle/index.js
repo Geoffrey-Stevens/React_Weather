25875ec4b700fb7a16916596f4a71de764aa0e39
Geoffrey_Stevens <geoffreycmstevens@gmail.com>
2019-03-06 11:44:48 -0500

    first commit

---
+++ src/components/weatherTitle/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+
+class WeatherTitle extends Component {
+  render() {
+    return (
+      <div className="WeatherTitle">
+        <h1> Check the Weather!</h1>
+      </div>
+    );
+  }
+}
+
+export default WeatherTitle;
