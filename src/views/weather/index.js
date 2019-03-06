2a8a2419b7ec248501de598a2d0370687d28f5a2
Geoffrey_Stevens <geoffreycmstevens@gmail.com>
2019-03-06 13:55:59 -0500

    added openweather API call, and kelvin conversion

--- src/views/weather/index.js
+++ src/views/weather/index.js
@@ -8,7 +8,7 @@ import API_KEY from '../../config.js';
 
 
 class Weather extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       'data': [],
@@ -16,13 +16,33 @@ class Weather extends Component {
     }
   }
 
+getWeather = async(e) => {
+  e.preventDefault();
+
+    let city = e.target.elements.city.value
+    let country = e.target.elements.country.value
+    if (country === '' || country === null) {
+      country = 'US';
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${this.state.API_KEY}`;
+
+    let response = await fetch(url);
+
+    let data = await response.json();
+
+    this.setState({'data': data});
+}
+
+
+
+
+
   componentWillMount(){
-  this.setstate({'API_KEY': API_KEY });
+  this.setState({'API_KEY': API_KEY });
   }
 
 
   render() {
-    console.log(this.state.API_KEY)
     return (
       <div className="row">
         <div className="col-md-4">
@@ -31,8 +51,8 @@ class Weather extends Component {
 
 
         <div className="col-md-8">
-          <WeatherForm/>
-          <WeatherInfo/>
+          <WeatherForm getWeather={this.getWeather}/>
+          <WeatherInfo data={this.state.data}/>
 
         </div>
 
